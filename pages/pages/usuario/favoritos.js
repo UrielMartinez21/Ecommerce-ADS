0ed0122b2718f0c694fe8c2e5822c590f7e485b1
Gabriela Campos <gabrielacampos1418@gmail.com>
2023-06-12 19:27:35 -0600

    corrccionFavoritos

--- pages/pages/usuario/favoritos.js
+++ pages/pages/usuario/favoritos.js
@@ -1,14 +1,20 @@
-import Layout from "@/layout/layout"
+import Layout from "@/layout/layout";
 import React, { useEffect, useState } from "react";
 import { DataView } from 'primereact/dataview';
 import { Button } from 'primereact/button';
+import { Message } from 'primereact/message';
 import { Image } from 'primereact/image';
 import { Tag } from 'primereact/tag';
+import { Dialog } from 'primereact/dialog';
 
 import { useRouter } from 'next/router';
+
 const Favoritos = () => {
   const router = useRouter();
-  const [flores, setFlores] = useState([])
+  const [flores, setFlores] = useState([]);
+  const [selectedFlor, setSelectedFlor] = useState(null);
+  const [confirmationDialogVisible, setConfirmationDialogVisible] = useState(false);
+  const [eliminarExitoso, setEliminarExitoso] = useState(false);
 
   useEffect(() => {
     const datosFlores = [
@@ -38,11 +44,9 @@ const Favoritos = () => {
         nombre: "VersicolorP", precio: 84.69, categoria: "verano", estatus: "Agotado",
         imagen: "https://img2.freepng.es/20180409/wce/kisspng-cut-flowers-iris-versicolor-iris-5acb7fdb4bc3d7.6318569115232859793103.jpg", descripcion: "Descripcion de versicolor"
       },
-    ]
-    setFlores(datosFlores)
-  }, [])
-
-
+    ];
+    setFlores(datosFlores);
+  }, []);
 
   const getSeverity = (flor) => {
     switch (flor.estatus) {
@@ -60,33 +64,28 @@ const Favoritos = () => {
     }
   };
 
-
-  const eliminar = (flor) => {
-    const filteredFlores = flores.filter((f) => f.nombre !== flor.nombre);
+  const eliminar = () => {
+    const filteredFlores = flores.filter((f) => f.nombre !== selectedFlor.nombre);
     setFlores(filteredFlores);
+    setSelectedFlor(null);
+    setConfirmationDialogVisible(false);
+    setEliminarExitoso(true);
   };
- 
+
+  const confirmarEliminar = (flor) => {
+    setSelectedFlor(flor);
+    setConfirmationDialogVisible(true);
+  };
 
   const itemTemplate = (flor) => {
-
-
-
-    const eliminar = (flor) => {
-      const filteredFlores = flores.filter((f) => f.nombre !== flor.nombre);
-      setFlores(filteredFlores);
-    };
-
     return (
-        <div className="col-12">
-
-        <div className="flex flex-column xl:flex-row xl:align-items-start p-4 gap-4 ">
-          <img className="w-9 sm:w-16rem xl:w-10rem shadow-2 block xl:block mx-auto border-round shadow-5 border-round" src={`https://media.admagazine.com/photos/61eb22cb9b19d943aa117b30/master/w_1600%2Cc_limit/Girasol.jpg`} alt="girasol" />
-          
+      <div className="col-12">
+        <div className="flex flex-column xl:flex-row xl:align-items-start p-4 gap-4">
+          <img className="w-9 sm:w-16rem xl:w-10rem shadow-2 block xl:block mx-auto border-round shadow-5 border-round" src={flor.imagen} alt="girasol" />
           <div className="flex flex-column sm:flex-row justify-content-between align-items-center xl:align-items-start flex-1 gap-4">
-            <div className="flex flex-column align-items-center sm:align-items-start ">
+            <div className="flex flex-column align-items-center sm:align-items-start">
               <div className="text-2xl font-bold text-900">{flor.nombre}</div>
               <span className="text-2xl font-semibold sh">${flor.precio}</span>
-              {/* <Rating value={flor.rating} readOnly cancel={false}></Rating> */}
               <div className="flex align-items-center gap-3">
                 <Tag value={flor.estatus} severity={getSeverity(flor)}></Tag>
                 <span className="flex align-items-center gap-2">
@@ -94,24 +93,15 @@ const Favoritos = () => {
                   <span className="font-semibold">{flor.categoria}</span>
                 </span>
               </div>
-             
-                
             </div>
             <div className="">
-                <h5>Descripción: </h5>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                 Ut enim ad minim veniam, quis nostrud exercitation </p> 
-              </div> 
+              <h5>Descripción: </h5>
+              <p>{flor.descripcion}</p>
+            </div>
             <div className="flex sm:flex-column align-items-center sm:align-items-end gap-3 sm:gap-2">
-              {/* <span className="text-2xl font-semibold">${flor.precio}</span> */}
-              
-              
-              <Button   label="Eliminar" onClick={() => eliminar(flor)} icon="pi pi-times" rounded severity="danger" aria-label="Favorite" className="p-button-rounded"  />
-              <Button label="Agregar" icon="pi pi-shopping-cart" className="font-light ml-2 "       severity="success"disabled={flor.estatus === 'Agotado'}></Button>
-              
-                
-            
-              
+            <Button label="Agregar" icon="pi pi-shopping-cart" className="font-light ml-2 " severity="success" disabled={flor.estatus === 'Agotado'}></Button>
+              <Button label="Eliminar" onClick={() => confirmarEliminar(flor)} icon="pi pi-trash" severity="danger" aria-label="Favorite" className="font-light ml-2" />
+           
             </div>
           </div>
         </div>
@@ -129,14 +119,23 @@ const Favoritos = () => {
           <h1>Favoritos</h1>
         </div>
         <div className="col-12">
+          {eliminarExitoso && <Message severity="success" text="Producto eliminad0 exitosamente"   />}
           <div className="card">
             <DataView value={flores} itemTemplate={itemTemplate} />
           </div>
         </div>
-        
       </div>
+      <Dialog visible={confirmationDialogVisible} onHide={() => setConfirmationDialogVisible(false)} breakpoints={{ '960px': '75vw' }} header="Eliminar de Favoritos" style={{ width: '30vw' }} footer={
+        <div className="p-dialog-footer">
+            <Button label="Eliminar" onClick={eliminar} security="danger"  />
+          <Button label="Cancelar" onClick={() => setConfirmationDialogVisible(false)} className="p-button-text" />
+        </div>
+      }>
+       
+        <p>¿Está seguro de que deseas eliminar el producto {selectedFlor?.nombre} de tus favoritos?</p>
+      </Dialog>
     </Layout>
-  )
-}
+  );
+};
 
-export default Favoritos
+export default Favoritos;
