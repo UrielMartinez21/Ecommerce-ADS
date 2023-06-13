9f43eef6ce4c9e22949d4a17e82af60c5b04ed08
UrielMartinez21 <uriellmartinezz35@gmail.com>
2023-06-13 04:05:00 -0600

    Nombre y logo dinamico

--- pages/pages/usuario/miCuenta.js
+++ pages/pages/usuario/miCuenta.js
@@ -3,11 +3,20 @@ import { Button } from 'primereact/button';
 import { Avatar } from 'primereact/avatar';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
+import { useEffect, useState } from "react";
 
 
 const miCuenta = () => {
-   //--> Variable de redireccinamiento
- const router = useRouter();
+  const [nombre, setNombre] = useState('')
+  const [icono, setIcono] = useState('')
+
+  useEffect(() => {
+    setNombre(localStorage.getItem('nombre'))
+    //--> Extraer primera letra del nombre
+    setIcono(localStorage.getItem('nombre').charAt(0))
+  }, [])
+  //--> Variable de redireccinamiento
+  const router = useRouter();
 
   return (
     <Layout
@@ -16,39 +25,40 @@ const miCuenta = () => {
       <div className="grid">
         <div className="col-12">
           <div className="card">
-            
+
             <h3> Mi Perfil</h3>
-            <div className=''> 
-              </div>
-              <div className='field'> 
+            <div className=''>
+            </div>
+            <div className='field'>
               <Link href="/pages/usuario/miCuenta" className="font-medium no-underline ml-2 text-right cursor-pointer" style={{ color: 'var(--primary-color)' }} >
-               Regresar
+                Regresar
               </Link>
-              </div>
+            </div>
             <div className="flex align-items-center flex-wrap">
-              <Avatar label="U" size="xlarge" shape="circle" className="flex align-items-center justify-content-center m-2" />
-              <p className="flex align-items-center justify-content-center m-2">Omarcito Gómez</p>
+              <Avatar
+                label={icono} size="xlarge" shape="circle" className="flex align-items-center justify-content-center m-2" />
+              <p className="flex align-items-center justify-content-center m-2">{nombre}</p>
             </div>
             <div className="mt-4">
               <div className="flex align-items-center flex-wrap my-2">
                 <i className="pi pi-user-edit flex align-items-center justify-content-center m-2" style={{ fontSize: '2rem' }}></i>
-                <Button label="Nombre Elegido" text className="flex align-items-center justify-content-center m-2" onClick={() => { router.push('/pages/usuario/modifDatos/modifNom')}} />
+                <Button label="Nombre Elegido" text className="flex align-items-center justify-content-center m-2" onClick={() => { router.push('/pages/usuario/modifDatos/modifNom') }} />
               </div>
               <div className="flex align-items-center flex-wrap my-2">
                 <i className="pi pi-lock align-items-center justify-content-center m-2" style={{ fontSize: '2rem' }}></i>
-                <Button label="Contraseña" text className="flex align-items-center justify-content-center m-2" onClick={() => { router.push('/pages/usuario/modifDatos/modifContrasena')}} />
+                <Button label="Contraseña" text className="flex align-items-center justify-content-center m-2" onClick={() => { router.push('/pages/usuario/modifDatos/modifContrasena') }} />
               </div>
               <div className="flex align-items-center flex-wrap my-2">
                 <i className="pi pi-phone flex align-items-center justify-content-center m-2" style={{ fontSize: '2rem' }}></i>
-                <Button label="Número de Teléfono" text className="flex align-items-center justify-content-center m-2" onClick={() => { router.push('/pages/usuario/modifDatos/modifTel')}} />
+                <Button label="Número de Teléfono" text className="flex align-items-center justify-content-center m-2" onClick={() => { router.push('/pages/usuario/modifDatos/modifTel') }} />
               </div>
               <div className="flex align-items-center flex-wrap my-2">
                 <i className="pi pi-map-marker flex align-items-center justify-content-center m-2" style={{ fontSize: '2rem' }}></i>
-                <Button label="Dirección" text className="flex align-items-center justify-content-center m-2" onClick={() => { router.push('/pages/usuario/modifDatos/addDir')}} />
+                <Button label="Dirección" text className="flex align-items-center justify-content-center m-2" onClick={() => { router.push('/pages/usuario/modifDatos/addDir') }} />
               </div>
               <div className="flex align-items-center flex-wrap my-2">
                 <i className="pi pi-credit-card align-items-center justify-content-center m-2" style={{ fontSize: '2rem' }}></i>
-                <Button label="Tarjetas" text className="flex align-items-center justify-content-center m-2" onClick={() => { router.push('/pages/usuario/modifDatos/modifTarjetas')}} />
+                <Button label="Tarjetas" text className="flex align-items-center justify-content-center m-2" onClick={() => { router.push('/pages/usuario/modifDatos/modifTarjetas') }} />
               </div>
 
             </div>
--- pages/pages/usuario/perfil.js
+++ pages/pages/usuario/perfil.js
@@ -2,12 +2,21 @@ import Layout from "@/layout/layout"
 import { Button } from 'primereact/button';
 import { Avatar } from 'primereact/avatar';
 import { useRouter } from 'next/router';
+import { useEffect, useState } from "react";
 
 
 const Perfil = () => {
 
- //--> Variable de redireccinamiento
- const router = useRouter();
+  //--> Variable de redireccinamiento
+  const router = useRouter();
+
+  const [nombre, setNombre] = useState('')
+  const [icono, setIcono] = useState('')
+  useEffect(() => {
+    setNombre(localStorage.getItem('nombre'))
+    //--> Extraer primera letra del nombre
+    setIcono(localStorage.getItem('nombre').charAt(0))
+  }, [])
 
   return (
     <Layout
@@ -18,8 +27,8 @@ const Perfil = () => {
           <div className="card">
             <h5>Perfil</h5>
             <div className="flex align-items-center flex-wrap">
-              <Avatar label="U" size="xlarge" shape="circle" className="flex align-items-center justify-content-center m-2" />
-              <p className="flex align-items-center justify-content-center m-2"> <span className="underline">Uriel</span></p>
+              <Avatar label={icono} size="xlarge" shape="circle" className="flex align-items-center justify-content-center m-2" />
+              <p className="flex align-items-center justify-content-center m-2"> <span className="underline">{nombre}</span></p>
             </div>
 
             <div className="mt-4">
@@ -29,7 +38,7 @@ const Perfil = () => {
               </div>
               <div className="flex align-items-center flex-wrap my-2">
                 <i className="pi pi-truck flex align-items-center justify-content-center m-2" style={{ fontSize: '2rem' }}></i>
-                <Button label="Seguimiento de pedidos" text className="flex align-items-center justify-content-center m-2" onClick={() => {router.push('/pages/seguimientoPedidos/seguirPedido')}} />
+                <Button label="Seguimiento de pedidos" text className="flex align-items-center justify-content-center m-2" onClick={() => { router.push('/pages/seguimientoPedidos/seguirPedido') }} />
               </div>
               <div className="flex align-items-center flex-wrap my-2">
                 <i className="pi pi-shopping-bag flex align-items-center justify-content-center m-2" style={{ fontSize: '2rem' }}></i>
@@ -41,12 +50,12 @@ const Perfil = () => {
               </div>
               <div className="flex align-items-center flex-wrap my-2">
                 <i className="pi pi-clock flex align-items-center justify-content-center m-2" style={{ fontSize: '2rem' }}></i>
-                <Button label="Historial" onClick={() => { router.push('/pages/usuario/miscompras') } } text className="flex align-items-center justify-content-center m-2" />
+                <Button label="Historial" onClick={() => { router.push('/pages/usuario/miscompras') }} text className="flex align-items-center justify-content-center m-2" />
               </div>
               <div className="flex align-items-center flex-wrap my-2">
                 <i className="pi pi-user flex align-items-center justify-content-center m-2" style={{ fontSize: '2rem' }} ></i>
-                <Button label="Mi cuenta" text className="flex align-items-center justify-content-center m-2"   onClick={() => { router.push('/pages/usuario/miCuenta') }}  />
-              
+                <Button label="Mi cuenta" text className="flex align-items-center justify-content-center m-2" onClick={() => { router.push('/pages/usuario/miCuenta') }} />
+
               </div>
               <div className="flex align-items-center flex-wrap my-2">
                 <i className="pi pi-list flex align-items-center justify-content-center m-2" style={{ fontSize: '2rem' }}></i>
