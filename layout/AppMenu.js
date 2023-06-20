e6b267be7a942816779c041e03d824fb380c825f
UrielMartinez21 <uriellmartinezz35@gmail.com>
2023-06-19 23:32:58 -0600

    Cambios visuales

--- layout/AppMenu.js
+++ layout/AppMenu.js
@@ -18,27 +18,27 @@ const AppMenu = () => {
   const model = [
     {
       label: 'Inicio',
-      items: [{ label: ' Ver Jardín del Edén', icon: "IoIosBasket", to: '/pages/dashboard' }]
+      items: [{ label: ' Ver Jardín del Edén', icon: "pi pi-fw pi-home", to: '/pages/dashboard' }]
     },
     {
       label: 'Catálogos',
       items: [
-        { label: 'Flores',  to: '/pages/catalogos/flores' },
-        { label: 'Peluches', icon: "fa-light fa-teddy-bear", to: '/pages/catalogos/peluches' },
-        {label: 'Eventos', icono: 'pi pi-fw pi-bookmark',
-          items: [{ label: 'Amor & Aniversario', icon: 'pi pi-fw pi-heart-fill', to: '/pages/catalogos/Eventos/amorAniv' },
-          { label: 'Agradecimiento', a: '/pages/catalogos/Eventos/agradece' },
-          { label: 'Bodas', a: '/pages/catalogos/Eventos/Boda' },
-          { label: 'Baby Shower', a: '/pages/catalogos/Eventos/babyShower' }, 
-          { label: 'Cumpleaños', a: '/pages/catalogos/Eventos/cumple' },
-          { label: 'Condolencias', a: '/pages/catalogos/Eventos/condolencias' }, 
-          { label: 'Graduación', a: '/pages/catalogos/Eventos/graduacion' },
-          {
-            label: 'Regalos',
-            items: [{ label: 'Para él', to: '/pages/catalogos/Eventos/paraEl' },
-            { label: 'Para ella', a: '/pages/catalogos/Eventos/paraElla' }]
-          },]
-        },
+        { label: 'Flores', icon: 'pi pi-fw pi-tablet', to: '/pages/catalogos/flores' },
+        { label: 'Peluches', icon: 'pi pi-fw pi-tablet', to: '/pages/catalogos/peluches' },
+        // {label: 'Eventos', icono: 'pi pi-fw pi-bookmark',
+        //   items: [{ label: 'Amor & Aniversario', icon: 'pi pi-fw pi-heart-fill', to: '/pages/catalogos/Eventos/amorAniv' },
+        //   { label: 'Agradecimiento', a: '/pages/catalogos/Eventos/agradece' },
+        //   { label: 'Bodas', a: '/pages/catalogos/Eventos/Boda' },
+        //   { label: 'Baby Shower', a: '/pages/catalogos/Eventos/babyShower' }, 
+        //   { label: 'Cumpleaños', a: '/pages/catalogos/Eventos/cumple' },
+        //   { label: 'Condolencias', a: '/pages/catalogos/Eventos/condolencias' }, 
+        //   { label: 'Graduación', a: '/pages/catalogos/Eventos/graduacion' },
+        //   {
+        //     label: 'Regalos',
+        //     items: [{ label: 'Para él', to: '/pages/catalogos/Eventos/paraEl' },
+        //     { label: 'Para ella', a: '/pages/catalogos/Eventos/paraElla' }]
+        //   },]
+        // },
         { label: 'Temporada', icon: 'pi pi-fw pi-tag', to: '/pages/catalogos/temporada' }
       ]
     },
@@ -48,7 +48,7 @@ const AppMenu = () => {
         { label: 'Personalizar arreglo', icon: 'pi pi-fw pi-gift', to: '/pages/usuario/personalizararreglo' },
         { label: 'Mis Compras', icon: 'pi pi-fw pi-shopping-bag', to: '/pages/usuario/compras' },
         { label: 'Mis favoritos', icon: 'pi pi-fw pi-heart', to: '/pages/usuario/favoritos' },
-        { label: 'Atención al cliente', icon: 'pi pi-fw pi-comments'},
+        { label: 'Atención al cliente', icon: 'pi pi-fw pi-comments' },
       ]
     }
 
--- pages/pages/pantallainicio/tokenresetear.js
+++ pages/pages/pantallainicio/tokenresetear.js
@@ -212,8 +212,9 @@ const TokenResetear = () => {
       </div>
 
       <Dialog header="Restablecer contraseña. " visible={mostrarDialogo} style={{ width: '50vw' }} onHide={() => setMostrarDialogo(false)}>
+        <p>Ingrese su nueva contraseña, este debe contener mínimo 6 caracteres de longitud.</p>
         <div className='flex justify-content-center my-2'>
-          <span>Ingrese su nueva contraseña, este debe contener mínimo 6 caracteres de longitud.</span> <br/>
+          <br />
           <Password
             id="cpassword" placeholder='Mínimo 6 caracteres'
             inputClassName={`w-full p-3 md:w-25rem`} className={`${estiloPassword} `}
